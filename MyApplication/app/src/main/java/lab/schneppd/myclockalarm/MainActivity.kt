2006ed66090d516bcd4f303a1fca4a4f4b06d4b1
david.schnepp <david.schnepp@schneppd.com>
2017-06-06 11:03:53 +0200

    Setup project

---
+++ MyApplication/app/src/main/java/lab/schneppd/myclockalarm/MainActivity.kt
@@ -0,0 +1,23 @@
+package lab.schneppd.myclockalarm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import kotlinx.android.synthetic.main.activity_main.*
+import android.view.View
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+    private fun ExecuteInputSetupClock(){
+        
+    }
+
+}
