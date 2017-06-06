0e3a76e2725022758045ebd6a8d5dd5b768be225
david.schnepp <david.schnepp@schneppd.com>
2017-06-06 16:55:49 +0200

    added alarm logic

--- MyApplication/app/src/main/java/lab/schneppd/myclockalarm/MainActivity.kt
+++ MyApplication/app/src/main/java/lab/schneppd/myclockalarm/MainActivity.kt
@@ -5,9 +5,20 @@ import android.os.Bundle
 
 import kotlinx.android.synthetic.main.activity_main.*
 import android.view.View
+import android.app.AlarmManager
+import android.content.Context
+import android.content.Context.ALARM_SERVICE
+import android.os.SystemClock
+import android.app.PendingIntent
+import android.content.Intent
+import android.opengl.Visibility
+import java.util.*
+import android.content.SharedPreferences
+import android.text.format.Time
 
 
 class MainActivity : AppCompatActivity() {
+    private var alarmMgr:AlarmManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +27,61 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun ExecuteInputSetupClock(){
-        
+    public fun ExecuteSetupClock(sender:View){
+        val hh:Int = timePicker.hour
+        val mm:Int = timePicker.minute
+
+        alarmMgr = this.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this, AlarmReceiver::class.java)
+        val alarmIntent:PendingIntent = PendingIntent.getBroadcast(applicationContext, 0, intent, 0)
+
+        var alarm:Calendar = Calendar.getInstance()
+        alarm.set(Calendar.HOUR, 4)
+        alarm.set(Calendar.MINUTE, 55)
+        alarm.set(Calendar.SECOND, 0)
+        alarm.set(Calendar.MILLISECOND, 0)
+
+        //set time for next tic
+
+
+
+        //save alarm for reboot
+        val sp:SharedPreferences = this.getPreferences(Context.MODE_PRIVATE)
+        var spe:SharedPreferences.Editor = sp.edit()
+        spe.putLong("Alarm", alarm.timeInMillis);
+        spe.commit();
+
+
+        //setup alarm
+        alarmMgr!!.set(AlarmManager.RTC_WAKEUP, alarm.timeInMillis, alarmIntent)
+
+        //update ui
+        btnClockSetup.visibility = View.GONE
+        btnClockCancel.visibility = View.VISIBLE
+
+        txtClockActivity.text = "Clock setup to tic in {$hh}h {$mm}m"
+
+    }
+
+    public fun ExecuteCancelClock(sender:View){
+        val intent = Intent(this, AlarmReceiver::class.java)
+        val alarmIntent:PendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0)
+
+        //cancel alarm
+        alarmMgr!!.cancel(alarmIntent)
+
+        //delete from memory
+        val sp:SharedPreferences = this.getPreferences(Context.MODE_PRIVATE)
+        var spe:SharedPreferences.Editor = sp.edit()
+        spe.remove("Alarm");
+        spe.commit();
+
+        //update ui
+        btnClockSetup.visibility = View.VISIBLE
+        btnClockCancel.visibility = View.GONE
+
+        txtClockActivity.text = "No alarm setup"
+
     }
 
 }
